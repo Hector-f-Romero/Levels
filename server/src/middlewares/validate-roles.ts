f189026a34d46c3cc9ddd1da3b23267500cce29e
Hector Romero <hector.f.romero28@gmail.com>
2023-01-07 13:18:23 -0500

    Validación de tipo de usuario como middleware realizado.

--- server/src/middlewares/validate-roles.ts
+++ server/src/middlewares/validate-roles.ts
@@ -1,5 +1,18 @@
-// const isAdminRole = (r)=>{
+import { NextFunction, Request, Response } from "express";
+import { UserType } from "../entities/User";
 
-// };
+const isAdminRole = (req: Request, res: Response, next: NextFunction) => {
+	const { idUser, userType } = res.locals.userData;
 
-// export{isAdminRole}
+	if (!userType) {
+		return res.status(400).json({ msg: "User data not found." });
+	}
+
+	if (!(UserType.ADMIN === userType)) {
+		return res.status(403).json({ msg: `The user ${idUser} doesn't have ADMIN role.` });
+	}
+
+	next();
+};
+
+export { isAdminRole };
--- server/src/routes/uploads.routes.ts
+++ server/src/routes/uploads.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { deleteFile, getFile, putFile, uploadFile } from "../controllers/uploads.controller";
 import { uploadFileMiddleware } from "../middlewares/upload-file";
+import { isAdminRole } from "../middlewares/validate-roles";
 import { verifyJWT } from "../middlewares/verify-jwt";
 import { fileExists, fileIdAndFolderExists } from "../validators/upload.validator";
 
@@ -10,6 +11,6 @@ router.get("/:folder/:id", [fileIdAndFolderExists], getFile);
 // router.post("/:folder/:id", upload.single("file"), uploadFile);
 router.post("/:folder/:id", [uploadFileMiddleware("file"), fileExists, fileIdAndFolderExists], uploadFile);
 router.put("/:folder/:id", [uploadFileMiddleware("file"), fileExists, fileIdAndFolderExists], putFile);
-router.delete("/:folder/:id", [verifyJWT, fileIdAndFolderExists], deleteFile);
+router.delete("/:folder/:id", [verifyJWT, isAdminRole, fileIdAndFolderExists], deleteFile);
 
 export default router;
