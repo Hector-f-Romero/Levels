1b898a8cba4facf7988423fb1d9b796c01e8375d
Hector Romero <hector.f.romero28@gmail.com>
2023-01-07 12:54:31 -0500

    Validación de usuario existente en la BD mediante el middleware de validación de JWT. La información del usuario se guarda ahora en res.locals.userData

--- server/src/helpers/error.handle.ts
+++ server/src/helpers/error.handle.ts
@@ -2,7 +2,7 @@ import { Response } from "express";
 
 const handleHttp = (res: Response, error: unknown, myMsg: string) => {
 	// console.log(error);
-	return res.status(500).json({ myMsg });
+	return res.status(500).json({ myMsg, error });
 };
 
 export { handleHttp };
--- server/src/middlewares/verify-jwt.ts
+++ server/src/middlewares/verify-jwt.ts
@@ -1,18 +1,37 @@
 import { NextFunction, Request, Response } from "express";
+
+import { AppDataSource } from "../config/mysql";
+import { User } from "../entities";
 import { handleHttp } from "../helpers/error.handle";
 import { verifyJWTLogic } from "../helpers/jwt.hanlde";
 
+const userRepository = AppDataSource.getRepository(User);
+
+/**
+ * Verify if the JWT is an object, the key "idUser" exists in this object and if the idUser exists in BD.
+ * @param req
+ * @param res
+ * @param next
+ */
 const verifyJWT = async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		const jwtByUser = req.headers.authorization || "";
 		const jwt = jwtByUser.split(" ").pop();
 		const resultValidation = verifyJWTLogic(`${jwt}`);
-		console.log(typeof resultValidation);
-		// console.log(resultValidation);
+
 		if (!(resultValidation instanceof Object)) {
 			throw new Error("Error occurred in validation of jwt (Not an object");
 		}
-		console.log("Ya pasó el IF");
+
+		if (!resultValidation.idUser) {
+			throw new Error("idUser doesn't exists in the JWT.");
+		}
+
+		const existsUser = await userRepository.findOneBy({ idUser: resultValidation.idUser });
+		if (!existsUser) {
+			throw new Error(`The user with id ${resultValidation.idUser} doesn't exist.`);
+		}
+		res.locals.userData = existsUser;
 		next();
 	} catch (error) {
 		handleHttp(res, error, "ERROR_VERIFY_TOKEN");
